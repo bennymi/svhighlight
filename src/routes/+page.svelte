7232ed83cfa90ea8b95170c3f3135142e2274b5e
bennymi <xbenmmusicx@gmail.com>
2023-02-02 21:13:02 +0100

    update scroll lines in documentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 		{ lines: '1', scrollLine: 0, text: '1: Import CodeBlock' },
 		{ lines: '3', scrollLine: 0, text: '2: Import CSS theme' },
 		{ lines: '5-7', scrollLine: 3, text: '3: Define code' },
-		{ lines: '11-16', scrollLine: 12, text: '4: Insert CodeBlock' }
+		{ lines: '10-15', scrollLine: 15, text: '4: Insert CodeBlock' }
 	];
 </script>
 
